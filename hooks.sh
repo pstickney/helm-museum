3bb7df5d61a23aaf27e4ca996ca0eb2eeb92a872
pstickney <770883+pstickney@users.noreply.github.com>
2020-08-10 12:53:08 -0400

    need to use raw content. yikes

--- hooks.sh
+++ hooks.sh
@@ -6,7 +6,7 @@ if [ "$HELM_DEBUG" == true ]; then
   set -x
 fi
 
-CHARTMUSEUM_VERSION_URL="https://github.com/pstickney/helm-museum/blob/master/CHARTMUSEUM_VERSION"
+CHARTMUSEUM_VERSION_URL="https://raw.githubusercontent.com/pstickney/helm-museum/master/CHARTMUSEUM_VERSION"
 CHARTMUSEUM_DARWIN_SHA="33363f7471968a983d3f52562398fb120cc9022595ce5d090a5870d34ec45088"
 CHARTMUSEUM_LINUX_SHA="53402edf5ac9f736cb6da8f270f6bbf356dcbbe5592d8a09ee6f91a2dc30e4f6"
 
