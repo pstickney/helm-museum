b450afc2b99ae5f409e6b2397124ed47c111ac3e
pstickney <770883+pstickney@users.noreply.github.com>
2020-08-11 05:32:29 -0400

    fixing another copy bug

--- hooks.sh
+++ hooks.sh
@@ -98,7 +98,7 @@ plugin_update () {
     pushd "$tmpDir" > /dev/null
     git clone "$HELM_MUSEUM_GIT_URL" .
     git checkout -b latest "$latestTag"
-    cp -a -f ./.* ./* "$HELM_PLUGIN_DIR/"
+    cp -a -f ./.[^.]* ./* "$HELM_PLUGIN_DIR/"
     popd > /dev/null
     rm -rf "$tmpDir"
   else
